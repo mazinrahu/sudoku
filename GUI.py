e413babc9967ff18da6affbc0452333bbb4b2764
mazinrahu <MazinRahu61@gmail.com>
2022-08-20 17:35:28 -0400

    Comments and Docstrings

--- GUI.py
+++ GUI.py
@@ -17,15 +17,25 @@ class Grid:
         self.selected = None
 
     def update_model(self):
+        """Updates the board with the new value 
+        """
         self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
     def place(self, val):
+        """Places the value on the board if it is valid
+
+        Args:
+            val (int): The value to be placed on the board
+
+        Returns:
+            Boolean: Can the value be placed on the board
+        """
         a = Sudoku("easy")
         row, col = self.selected
         if self.cubes[row][col].value == 0:
             self.cubes[row][col].set(val)
             self.update_model()
-
+            #Checks for the validity of the value in the given cube
             if a.valid_pos(self.model, val, (row,col)) and a.solve(self.model):
                 return True
             else:
@@ -35,10 +45,20 @@ class Grid:
                 return False
 
     def sketch(self, val):
+        """Draws the temporary value given to the sudoku board before the enter key is pressed
+
+        Args:
+            val (int): The temporary value given to the sudoku board
+        """
         row, col = self.selected
         self.cubes[row][col].set_temp(val)
 
     def draw(self, win):
+        """Draws the grid and cubes on the given surface
+
+        Args:
+            win : The surface to be drawn on
+        """
         # Draw Grid Lines
         gap = self.width / 9
         for i in range(self.rows+1):
@@ -55,6 +75,12 @@ class Grid:
                 self.cubes[i][j].draw(win)
 
     def select(self, row, col):
+        """Selects the specific cube on the board
+
+        Args:
+            row (int): The row index of the cube to be selected
+            col (int): The col index of the cube to be selected
+        """
         # Reset all other
         for i in range(self.rows):
             for j in range(self.cols):
@@ -64,14 +90,20 @@ class Grid:
         self.selected = (row, col)
 
     def clear(self):
+        """Removes the number added to the ube by pressing the Delete key 
+        """
         row, col = self.selected
         if self.cubes[row][col].value == 0:
             self.cubes[row][col].set_temp(0)
 
     def click(self, pos):
-        """
-        :param: pos
-        :return: (row, col)
+        """Returns the position of the cube that has been clicked on
+
+        Args:
+            pos (tuple): The position of the mouse click 
+
+        Returns:
+            tuple: (row,col) of the cube which has been clicked on
         """
         if pos[0] < self.width and pos[1] < self.height:
             gap = self.width / 9
@@ -82,6 +114,11 @@ class Grid:
             return None
 
     def is_finished(self):
+        """SHows if the game ended when there are no more empty spaces left
+
+        Returns:
+            Boolean: Has the game ended?
+        """
         for i in range(self.rows):
             for j in range(self.cols):
                 if self.cubes[i][j].value == 0:
@@ -90,43 +127,63 @@ class Grid:
 
 
 class Cube:
-    rows = 9
-    cols = 9
-
     def __init__(self, value, row, col, width ,height):
         self.value = value
         self.temp = 0
-        self.row = row
-        self.col = col
+        self.row = 9
+        self.col = 9
         self.width = width
         self.height = height
         self.selected = False
 
     def draw(self, win):
+        """Draws the red square around the cube to show it is selected. It also draws the number in the cube.
+
+        Args:
+            win : The surface on which things have to be drawn
+        """
         fnt = pygame.font.SysFont("comicsans", 40)
 
         gap = self.width / 9
         x = self.col * gap
         y = self.row * gap
 
+        #Enters the temporary value given by the user until the Enter key is pressed
         if self.temp != 0 and self.value == 0:
             text = fnt.render(str(self.temp), 1, (128,128,128))
             win.blit(text, (x+5, y+5))
-        elif not(self.value == 0):
+        #Sets the correct value when the user presses enter    
+        elif self.value != 0:
             text = fnt.render(str(self.value), 1, (0, 0, 0))
             win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
-
+        #Draws a red square around the box which is selected
         if self.selected:
             pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
 
     def set(self, val):
+        """Sets the value in the cube
+
+        Args:
+            val (int): The number entered for the cube
+        """
         self.value = val
 
     def set_temp(self, val):
+        """Temporarily sets the value in the cube until the enter key is pressed
+
+        Args:
+            val (int): The number entered for the given cube
+        """
         self.temp = val
 
 
 def redraw_window(win, board):
+    """Draws the grid and board with the values
+
+    Args:
+        win : The surface where the window has to be drawn
+        board (List): The Sudoku board
+    """
     win.fill((255,255,255))
     # Draw grid and board
     board.draw(win)
@@ -134,16 +191,19 @@ def redraw_window(win, board):
 
 
 def main(diff):
+    """The main function run the game and all the key presses
+
+    Args:
+        diff (str): The difficulty at which the Sudoku game runs
+    """
     win = pygame.display.set_mode((540,600))
     pygame.display.set_caption("Sudoku")
     board = Grid(9, 9, 540, 540, diff)
     key = None
     run = True
-    start = time.time()
-    strikes = 0
     while run:
 
-
+        #All the key presses needed to play the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -176,13 +236,12 @@ def main(diff):
                             print("Success")
                         else:
                             print("Wrong")
-                            strikes += 1
                         key = None
 
                         if board.is_finished():
                             print("Game over")
                             run = False
-
+            #The mouse click to select the cube
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 clicked = board.click(pos)
--- sudoku.py
+++ sudoku.py
@@ -18,8 +18,8 @@ class Sudoku:
         nums =[1,2,3,4,5,6,7,8,9]
         shuffle(nums)
         x = self.b
-        x[0] = nums
-        return self.solve(x)
+        x[0] = nums  #Sets a shuffled nums as first row of x
+        return self.solve(x) #Solves x with the first row in it to generate a sudoku board
 
     def remove(self):
         """Removes random numbers from the Sudoku board based pn the difficulty 
@@ -27,7 +27,7 @@ class Sudoku:
         Returns:
             List: Sudoku board with random values removed
         """
-        if self.d == "easy":
+        if self.d == "easy": #Decides the number of empty spaces in the board based on difficulty
             a = 81 - randint(50, 60)
         elif self.d == "medium":
             a = 81 - randint(40, 50)
@@ -36,7 +36,7 @@ class Sudoku:
 
         x = self.b
         count = 0
-        while count != a:
+        while count != a: #Sets random numbers to zero
             row = randint(0,8)
             col = randint(0,8)
             if x[row][col] != 0:
@@ -63,9 +63,9 @@ class Sudoku:
         
         for i in range(1,10):
             if self.valid_pos(x, i, (row, col)):
-                x[row][col] = i
+                x[row][col] = i #Puts in the number in the given position if its not already in the row column or box
 
-                if self.solve(x):
+                if self.solve(x): #Uses recursion on the given board with new value added to it
                     return x
 
                 x[row][col] = 0
@@ -83,15 +83,15 @@ class Sudoku:
         """
         
         for i in range(9):
-            if board[cell[0]][i] == num and cell[1] != i:
+            if board[cell[0]][i] == num and cell[1] != i: #Checks the row for validity
                 return False
-            if board[i][cell[1]] == num and cell[0] != i:
+            if board[i][cell[1]] == num and cell[0] != i: #Checks the column for validity
                 return False
 
             box_x = cell[0] // 3
             box_y = cell[1] // 3
 
-            for i in range(box_x*3, box_x * 3 + 3):
+            for i in range(box_x*3, box_x * 3 + 3): #Checks a 3 x 3 box for the validity of given value
                 for j in range(box_y*3, box_y*3 + 3):
                     if board[i][j] == num and cell != (i,j):
                         return False
